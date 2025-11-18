083429578c0da2c3f49964897e9a7feebc4a051c
sbrisbane <31763139+sbrisbane@users.noreply.github.com>
2025-11-18 09:55:01 +0000

    Add MNIST classification script using JAX and Keras

---
+++ jupyter/mnist-jax.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+#### CELL1 #################
+#!nvidia-smi
+############################
+
+#### CELL2 #################
+import os
+os.environ["KERAS_BACKEND"] = "jax"
+from matplotlib import pyplot as plt
+import numpy as np
+import keras
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# Load the data and split it between train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+
+first_image = np.array(x_train[0], dtype='float')
+pixels = first_image.reshape((28, 28))
+plt.imshow(pixels, cmap='gray')
+plt.show()
+############################
+
+#### CELL3 #################
+
+import jax
+import jax.numpy as np
+print (jax.devices())
+
+import keras
+from keras import Sequential
+from keras import layers
+
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# Load the data and split it between train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+
+
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape),
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dropout(0.5),
+        layers.Dense(num_classes, activation="softmax"),
+    ]
+)
+
+model.summary()
+
+batch_size = 128
+epochs = 15
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
+############################
+
+
