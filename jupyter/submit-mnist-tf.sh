99f0819dfa83358b2394b366f0b85da87d743dfa
sbrisbane <31763139+sbrisbane@users.noreply.github.com>
2026-06-29 16:05:41 +0100

    Change script to run mnist-jax.py instead of mnist-tf.py

--- jupyter/submit-mnist-tf.sh
+++ jupyter/submit-mnist-tf.sh
@@ -10,4 +10,4 @@ module load miniconda3
 conda activate tf-keras
 #if nvcc is installed onlyin conda environments
 export XLA_FLAGS="--xla_gpu_cuda_data_dir=$CONDA_PREFIX"
-./mnist-tf.py
+./mnist-jax.py
