8bdbbe6175f3143839e97c72cbadcf6fc87ecdf4
sbrisbane <31763139+sbrisbane@users.noreply.github.com>
2026-06-29 16:05:20 +0100

    Create submit-mnist-tf.sh for SLURM job submission
    Added a bash script to submit MNIST TensorFlow jobs with SLURM.

---
+++ jupyter/submit-mnist-tf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p gpuq-short
+
+#REQUIREMENTS
+#conda create -n tf-keras
+#conda activate tf-keras
+#conda install -c nvidia -c conda-forge -c defaults matplotlib keras tensorflow-gpu ##conda install -c nvidia -c conda-forge -c defaults nvidia::cuda-toolkit nvidia::cuda-nvcc
+
+module load miniconda3
+conda activate tf-keras
+#if nvcc is installed onlyin conda environments
+export XLA_FLAGS="--xla_gpu_cuda_data_dir=$CONDA_PREFIX"
+./mnist-tf.py
