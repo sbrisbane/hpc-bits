b39579dbb66e75c2f2ca8967c0827f48db24efb1
sbrisbane <31763139+sbrisbane@users.noreply.github.com>
2026-09-17 17:17:57 +0100

    Support for shared MIG/GPU with jax

--- jupyter/submit-mnist-jax.sh
+++ jupyter/submit-mnist-jax.sh
@@ -13,6 +13,9 @@
 export XLA_FLAGS="--xla_gpu_cuda_data_dir=$CONDA_PREFIX"
 
 export KERAS_BACKEND=jax
+##Set this if you need to share the MIG slice (eg for slighyl less than 1/3 of the memory)
+#os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.3"
+#XLA_PYTHON_CLIENT_MEM_FRACTION="0.3"
 module load miniconda3
 conda activate jax-keras
 ./mnist-jax.py
